c585b00c0e818d69279107518cef8d3849ebef99
sabelo <67804592+SabeloMkhwanzi@users.noreply.github.com>
2022-01-20 00:26:01 +0200

    refactor index

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,49 +11,10 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Next.js, Typescript, Tailwindcss & Chakra-Ui Template</title>
-        <meta name="description" content="Web3.0 Template" />
+        <title>Everipedia-InstagramUI-Test</title>
+        <meta name="description" content="Everipedia-InstagramUI-Test" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to{" "}
-          <a href="https://nextjs.org">Next.js, TypeScript & Chakra-Ui!</a>
-        </h1>
-        <p className={styles.description}>
-          Get started by editing the Template
-        </p>
-
-        <Box my={5}>
-          <Button bg="blue.300" onClick={toggleColorMode}>
-            Color Mode
-          </Button>
-        </Box>
-
-        <Box>
-          <Button
-            borderRadius="full"
-            bgColor="blue.900"
-            onClick={connectWallet}
-          >
-            <p className="text-white text-base font-semibold">Connect Wallet</p>
-          </Button>
-        </Box>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 }
